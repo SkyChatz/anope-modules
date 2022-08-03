e0fa4f2d7f3122d6007de3111937814c68053abf
SkyChatz <94823237+SkyChatz@users.noreply.github.com>
2022-08-03 15:42:18 +0800

    Create bs_rps.cpp
    Base on Atheme rps game https://github.com/atheme-legacy/atheme-6.0/blob/master/modules/gameserv/rps.c

---
+++ bs_rps.cpp
@@ -0,0 +1,16 @@
+/* BotServ: Rps (Rock Paper Scissors)
+ *
+ * (C) 2022 SkyChatz
+ * Contact me 
+ * Email : SkyChatz1234@hotmail.com
+ * IRC : /s irc.skychatz.org:7000 #module
+ *
+ * You may use this module under the terms of docs/LICENSE, in the Anope source directory
+ *
+ * Configuration:
+ * module { name = "bs_rps"; }
+ * command { name = "RPS"; service = "BotServ"; command = "botserv/rps"; }
+ * fantasy { name = "RPS"; command = "botserv/rps"; prepend_channel = true; }
+ */
+
+#include "module.h"
